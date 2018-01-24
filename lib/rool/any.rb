7cb3b5c49820f49cbce59abf9b2fd2f664abbffb
Rolf <rdwoolen@gmail.com>
2018-01-23 19:31:26 -0700

    any and all now working and testing

--- lib/rool/any.rb
+++ lib/rool/any.rb
@@ -1,4 +1,7 @@
 module Rool
   class Any < Container
+  	def process(dataset={})
+      @children.any?{|r| r.process(dataset)}
+  	end
   end
 end
--- spec/lib/rool/not_spec.rb
+++ spec/lib/rool/not_spec.rb
@@ -2,13 +2,17 @@
 describe "Rool::Not" do
   describe '#process' do
     it 'raises an exception if more than one child rule is passed to the constructor' do
-      expect(Rool::Not.new(Rool::False.new, Rool::True.new)).to raise_exception
+      expect{Rool::Not.new(Rool::False.new, Rool::True.new).process}.to raise_exception(ArgumentError)
     end
     it 'returns true if the child rules process to false' do
       expect(Rool::Not.new(Rool::False.new).process).to be(true)
     end
     it 'returns false if the child rules process to true' do
       expect(Rool::Not.new(Rool::True.new).process).to be(false)
+    end
+    it 'returns true if the child rule processes to false' do
+      @data = {foo: 5}
+      expect(Rool::GreaterThan.new(:foo, 10).process(@data)).to eq(true)
     end
   end
 end
