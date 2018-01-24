447401b097652fabb29c62b23ea5e61e6972cb8f
westnorthern <rrichardson610@gmail.com>
2018-01-23 19:07:47 -0700

    defined Rool::Not process

--- lib/rool/not.rb
+++ lib/rool/not.rb
@@ -1,4 +1,18 @@
 module Rool
   class Not < Container
+  	def process(dataset={})
+  		if @children.length != 1
+  			raise "Rool::Not can only accept one argument"
+  		else
+  			if @children[0].process == true
+  				puts "flippin it to false"
+  				return false
+  			elsif @children[0].process == false
+  				puts "flippin it to true"
+  				return true
+  			end
+  		end
+  		puts "wtf?"
+    end
   end
 end
