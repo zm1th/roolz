0dad586d62bb570d8f5449b73d4715e3717454b6
westnorthern <rrichardson610@gmail.com>
2018-01-23 20:03:55 -0700

    fixes Rool::Regex

--- lib/rool/regex.rb
+++ lib/rool/regex.rb
@@ -1,4 +1,11 @@
 module Rool
   class Regex < Basic
+  	def process(dataset)
+  		super
+  		if @operand.respond_to?(:match?) && dataset[@data_key].respond_to?(:<)
+  			return @operand.match?(dataset[@data_key])
+  		end
+  		false
+  	end
   end
 end
