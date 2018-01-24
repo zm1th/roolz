9657fff28be1115c52a1980850aed3f56ba6440e
westnorthern <rrichardson610@gmail.com>
2018-01-24 16:10:16 -0700

    test for Rool::Email R&M

--- spec/lib/rool/email_spec.rb
+++ spec/lib/rool/email_spec.rb
@@ -2,18 +2,32 @@ describe "Rool::Email" do
   before :each do
     @data = {
       foo: 'ryarthur262@gmail.com',
-      fizz: 'Buzz',
+      fizz: '@gmail.ryan',
       cool_number: 262
     }
   end
 
 
   describe '#process' do
+
+    ## Expect True
+
     it 'returns true if the datakeys value is probably an e-mail address' do
       expect(Rool::Email.new(:foo).process(@data)).to be(true)
     end
+
+    ## Expect False
+    
     it 'returns false if the datakeys value is probably not an e-mail address' do
       expect(Rool::Email.new(:fizz).process(@data)).to be(false)
+    end
+
+    ## Result and Message Attributes
+
+    it 'will have a false result attribute and a message after a false return' do
+      @false_test = Rool::Email.new(:fizz)
+      @false_test.process(@data)
+      expect(@false_test).to have_attributes(result: false, message: "#{@data[:fizz]} is probably not an e-mail address.")
     end
   end
 end
