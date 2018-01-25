6f6e46274ba59f0c59082b7e0a55ee8bcbeba6aa
Andrew Littlefield <andrew.ray.allan@gmail.com>
2018-01-25 11:08:24 -0700

    fixed less_than and not specs

--- lib/rool/not.rb
+++ lib/rool/not.rb
@@ -1,4 +1,18 @@
 module Rool
   class Not < Container
+  	def process(dataset={})
+  		if @children.length > 1
+  			raise "Only one child rule may be passed"
+  		end
+  		#find a way to single out the true/false from expect.
+  		#@children comes through as an array, index 0 should bring out true/false.
+  		if @children[0].process(dataset) == true
+  			return false
+  		elsif @children[0].process(dataset) == false
+  			return true
+  		end
+  		#It worked!
+  	end
   end
 end
+
--- spec/lib/rool/not_spec.rb
+++ spec/lib/rool/not_spec.rb
@@ -2,7 +2,7 @@
 describe "Rool::Not" do
   describe '#process' do
     it 'raises an exception if more than one child rule is passed to the constructor' do
-      expect(Rool::Not.new(Rool::False.new, Rool::True.new)).to raise_exception
+      expect{Rool::Not.new(Rool::False.new, Rool::True.new).process}.to raise_exception("Only one child rule may be passed")
     end
     it 'returns true if the child rules process to false' do
       expect(Rool::Not.new(Rool::False.new).process).to be(true)
